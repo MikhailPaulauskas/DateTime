f3222158073895aa8f11e9eabe6d5e585eecfdea
MikhailPaulauskas <w3bsew@yandex.ru>
2023-01-17 21:02:29 +0300

    added koooling

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,9 @@ public class Main {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm MM/dd");
         LocalDateTime now = LocalDateTime.now();
-
-
+        int koolling  = 23;
         System.out.println(now.format(formatter) + "now");
-        
+
 
         
 
